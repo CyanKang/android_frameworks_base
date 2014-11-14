a7e6709abdbb3d962dc0e0bb58ba5260e8b1e754
Darescu Ionut <ionut.darescu@gmail.com>
2014-11-14 04:24:58 -0500

    Base: AdBlocker [1/3]
    Change-Id: I4bf5f55021cf234e47cf344d0183b24b1550ca4e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3398,6 +3398,12 @@ public final class Settings {
         public static final String FORMAL_TEXT_INPUT = "formal_text_input";
 
         /**
+         * Disable ads (HFM)
+         * @hide
+         */
+        public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
