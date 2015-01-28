d1abfd701abe6336d3bb22c0bf1bb1c7295a048b
Schubi <schubi@erlangen.ccc.de>
2015-01-28 22:06:46 +0100

    Fix Derps
    Change-Id: Ib5fd4bd12e74bc99cefe1140a05edb781197e143

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3583,25 +3583,10 @@ public final class Settings {
         public static final String SCREEN_ON_NOTIFICATION_LED = "screen_on_notification_led";
 
         /**
-          * Volume keys control cursor in text fields (default is 0)
-          * 0 - Disabled
-          * 1 - Volume up/down moves cursor left/right
-          * 2 - Volume up/down moves cursor right/left
-          * @hide
-          */
-         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
-
-        /**
          * Override and forcefully disable the fullscreen keyboard
          * @hide
          */
         public static final String DISABLE_FULLSCREEN_KEYBOARD = "disable_fullscreen_keyboard";
-
-        /**
-         * Whether to show the IME switcher in the status bar
-         * @hide
-         */
-        public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
 
         /**
          * Automatic keyboard rotation timeout.  0 to disable completely.
